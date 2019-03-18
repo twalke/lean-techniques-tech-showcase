6a42d4f1150cdf820919cc67822e50a8484aff86
Tyler Walke <tyler.walke@gmail.com>
2019-03-17 22:12:38 -0600

    finished
    polish, testing

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const album = require("./album");
 main();
 
 async function main() {
-  while (true){
+  console.log("enter an album number between 1-100 to view ids and titles for that album. 'q' quits the program.")
+  while (true) {
     let input = rs.question("photo-album ");
-    if (input == "q" ){ break; }
+    if (input == "q" ) { break; }
     await album.getAlbum(input);
   }
 }
+
+module.exports = main;
--- index.test.js
+++ index.test.js
@@ -1,1 +1,5 @@
 const main = require("./index");
+
+test("test if object is returned", function() {
+  expect(typeof main()).toBe("object")
+})
