02dc0575d6e2fef1d4a72590decfe11b7b35ae9c
Tyler Walke <tyler.walke@gmail.com>
2019-03-20 21:52:58 -0600

    functions
    Created Album class, split main function into axios call, album
    builder, and album printer functions. Cleaned some things.

--- album.js
+++ album.js
@@ -1,24 +1,43 @@
 const axios = require("axios");
 
+class Album {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
+  }
+}
+
 module.exports = {
   getAlbum: async function (input) {
     let concatAlbumToUrl = "https://jsonplaceholder.typicode.com/photos?albumId=" + input;
-
     let request = axios.get(concatAlbumToUrl);
     await request;
-
+    return request;
+  },
+  buildAlbum: async function (request) {
+    let theAlbum = [];
     request.then(function(response) {
       let myAlbum = response.data;
       if (myAlbum.length == 0) {
-        console.log("sorry no album there");
+        console.log("Sorry, no album there.");
         return;
       }
       for (let i = 0; i < myAlbum.length; i++) {
-        console.log("[" + myAlbum[i].id + "] " + myAlbum[i].title);
+        let newId = myAlbum[i].id;
+        let newTitle = myAlbum[i].title;
+        let newPhoto = new Album(newId, newTitle);
+        theAlbum.push(newPhoto);
       }
     })
     .catch(function(err) {
       console.log(err);
     })
+    await request;
+    return theAlbum;
+  },
+  printAlbum: function (theAlbum) {
+    for (let i = 0; i < theAlbum.length; i++) {
+        console.log("[" + theAlbum[i].id + "] " + theAlbum[i].title);
+      }
   }
 };
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const rs = require("readline-sync");
-
 const album = require("./album");
 
 main();
 
 async function main() {
-  console.log("enter an album number between 1-100 to view ids and titles for that album. 'q' quits the program.")
+  console.log("Enter an album number between 1-100 to view ids and titles for that album. Entering 'q' quits the program.")
   while (true) {
     let input = rs.question("photo-album ");
     if (input == "q" ) { break; }
-    await album.getAlbum(input);
+
+    let request = album.getAlbum(input);
+    await request;
+
+    let myAlbum = album.buildAlbum(request);
+
+    myAlbum.then(function(result) {
+       album.printAlbum(result);
+    })
+    await myAlbum;
   }
 }
 
